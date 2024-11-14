a2cfaa634168666336c98490a675b5fa48943d32
a <artem.myasoedov@gmail.com>
2024-11-14 17:49:28 +0300

    Created two empty modules (api and client)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-    id("groovy")
+	id("groovy")
 }
 
 group = "xchng.mya.su"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.apache.groovy:groovy:4.0.14")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	implementation("org.apache.groovy:groovy:4.0.14")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
